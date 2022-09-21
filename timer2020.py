6a5de86a49bad5cf22b9a34d9c302cbd5627ad7a
donthaveausername <101548078+donthaveausername@users.noreply.github.com>
2022-09-21 17:30:18 +0800

    Update timer2020.py

--- timer2020.py
+++ timer2020.py
@@ -5,4 +5,4 @@ def reminder():
     notification.notify(title = "break reminder", message="break time",timeout = 1)
 while True:
     reminder()
-    time.sleep(1200)
+    time.sleep(1200) #time in seconds, edit the number to change the interval between breaks
